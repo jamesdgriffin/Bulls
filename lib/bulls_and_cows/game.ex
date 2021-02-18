637cbbbed950de22812740f39d9e4215d28bb011
griffinjames1 <57371137+griffinjames1@users.noreply.github.com>
2021-02-17 20:02:28 -0500

    get results in elixir

--- lib/bulls_and_cows/game.ex
+++ lib/bulls_and_cows/game.ex
@@ -22,14 +22,13 @@ defmodule BullsAndCows.Game do
 
   def get_random() do
     # gets the random 4 digit num for secret
-    d1 = to_string(Enum.random([0,1,2,3,4,5,6,7,8,9]))
-    d2 = to_string(Enum.random([0,1,2,3,4,5,6,7,8,9]))
-    d3 = to_string(Enum.random([0,1,2,3,4,5,6,7,8,9]))
-    d4 = to_string(Enum.random([0,1,2,3,4,5,6,7,8,9]))
+    d1 = Enum.random([0,1,2,3,4,5,6,7,8,9])
+    d2 = Enum.random([0,1,2,3,4,5,6,7,8,9])
+    d3 = Enum.random([0,1,2,3,4,5,6,7,8,9])
+    d4 = Enum.random([0,1,2,3,4,5,6,7,8,9])
 
-    str = d1 <> d2 <> d3 <> d4
-    {rand, _} = Integer.parse(str)
-    rand
+    str = [d1,d2,d3,d4]
+    str
   end
 
   def guess(st, gs) do
@@ -38,11 +37,20 @@ defmodule BullsAndCows.Game do
 
   end
 
-  def get_results(gs, sec) do
-    bulls = 0
-    cows = 0
-
+  def get_results(st, gs) do
+    index = 0
+    Enum.map(gs, fn(g) -> check_char(st.secret, g, index = index + 1) end)
   end
 
+  def check_char(sec, char, index) do
+    Enum.map(sec, fn(s) ->
+      cond do
+        (s == char) ->
+          "equals" <> to_string(index)
+        (s != char) ->
+          "dne" <> to_string(index)
+      end
+    end)
+  end
 
 end
